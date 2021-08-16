74549e480476d51847cd1056be12ca4a2fbbe29d
oachq <alan_oscar52@hotmail.com>
2021-08-16 01:27:19 -0700

    modificaciones para la seccion 15

--- controllers/auth.js
+++ controllers/auth.js
@@ -102,10 +102,13 @@ const renewToken = async(req, res = response) => {
     // Generar el TOKEN - JWT
     const token = await generarJWT(uid);
 
+    // obtener el usuario por UID
+    const usuario = await Usuario.findById(uid);
 
     res.json({
         ok: true,
-        token
+        token,
+        usuario
     });
 
 }
